3cef4292f024a75ce78ff59c5aa52692389f398f
Tomáš Stýblo <119765404+Tomiczech77@users.noreply.github.com>
2023-07-23 12:24:44 +0200

    úklid a úprava výpisu počtu zbývajících úkolů na stránku

--- toDoApp.js
+++ toDoApp.js
@@ -6,23 +6,26 @@ let myToDos = [{
     completion: false
 }, {
     text: "Uklidit",
-    completion: true
+    completion: false
 }, {
     text: "Nakrmit psa",
     completion: true
 }, {
     text: "Nakrmit kočku",
-    completion: false
+    completion: true
 }]
 
-// Vypíše počet, kolik zbývá dokončit úkolů
-let toDoLeft = myToDos.filter(function(oneToDo){
-    return oneToDo.completion === false
-})
+//****** Vypíše počet, kolik zbývá dokončit úkolů *****/
+// let toDoLeft = myToDos.filter(function(oneToDo){
+//     return oneToDo.completion === false
+// })
 
-const paragraph = document.createElement("p")
-paragraph.textContent = `Ještě zbývá udělat úkolů: ${toDoLeft.length}`
-document.querySelector("main").appendChild(paragraph)
+// const paragraph = document.createElement("p")
+// paragraph.textContent = `Ještě zbývá udělat úkolů: ${toDoLeft.length}`
+// document.querySelector("main").appendChild(paragraph)
+
+
+
 
 // Vypíše všechny úkoly do odstavců a zobrazí je na stránce
 for(let i=0; i<myToDos.length; i++){
@@ -53,10 +56,23 @@ let renderToDos = function(ourToDos, weSearching){
     let ourResult = ourToDos.filter(function(oneToDo){
         return oneToDo.text.toLowerCase().includes(weSearching.searchingText.toLowerCase())
     })
-    // promazání odstavců s úkoly
+    // Promazání počítání a vypisování počtu zbývajících úkolů z filtrování
+    document.querySelector("#to-dos-left").innerHTML = ""
+
+    // Počítání zbývajících úkolů z filtrování
+    let leftToDos = ourResult.filter(function(oneToDo){
+        return oneToDo.completion === false
+    })
+    // Vypisování počtu zbývajících úkolů z filtrování na stránce
+    let paragraph = document.createElement("p")
+    paragraph.textContent = `Ještě nám zbývá udělat: ${leftToDos.length}`
+    document.querySelector("#to-dos-left").appendChild(paragraph)
+
+
+    // Promazání odstavců s úkoly
     document.querySelector("#results-todos").innerHTML = ""
 
-    // vypsání úkolů na stránku podle vyhledávání
+    // Vypsání úkolů na stránku podle vyhledávání
     ourResult.forEach(function(oneResult){
         let paragraph = document.createElement("p")
         paragraph.textContent = oneResult.text
